23acc2ae60c6e81d5785c8154af9b0833ad1d67e
Vivek H <viveklaks@gmail.com>
2022-01-08 23:54:41 +0530

    added add note post api

--- src/main/java/com/example/demo/controller/NotesController.java
+++ src/main/java/com/example/demo/controller/NotesController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +31,11 @@ public class NotesController {
 	@GetMapping("/api/notes")
 	public ResponseEntity<List<Note>> readNotes(){
 		return new ResponseEntity<List<Note>>(nRepo.findAll(),HttpStatus.OK);
+				
+	}
+	@PostMapping("/api/notes")
+	public ResponseEntity<Note> createNotes(@RequestBody Note note){
+		return new ResponseEntity<Note>(nRepo.save(note),HttpStatus.OK);
 				
 	}
 
--- src/main/java/com/example/demo/entity/Note.java
+++ src/main/java/com/example/demo/entity/Note.java
@@ -1,10 +1,16 @@
 package com.example.demo.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.Data;
 
@@ -20,5 +26,13 @@ public class Note {
 	private String title;
 	private String body;
 	private String category;
+	
+	@Column(name="created_at",nullable=false, updatable=false)
+	@CreationTimestamp
+	private Date createAt;
+	
+	@Column(name="updated_at")
+	@UpdateTimestamp
+	private Date updatedAt;
 
 }
