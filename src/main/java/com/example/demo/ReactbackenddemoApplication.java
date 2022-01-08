1239c6df0d15670b98a0aad33d9a6abe12a99188
Vivek H <viveklaks@gmail.com>
2022-01-08 17:44:13 +0530

    first commit

---
+++ src/main/java/com/example/demo/ReactbackenddemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactbackenddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactbackenddemoApplication.class, args);
+		System.out.println("works fine");
+	}
+
+}
