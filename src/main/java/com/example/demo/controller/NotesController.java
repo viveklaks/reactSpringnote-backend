3719000f3498c8421b64fe015bd1ba48410c276f
Vivek H <viveklaks@gmail.com>
2022-01-09 17:55:17 +0530

    delete note by id

--- src/main/java/com/example/demo/controller/NotesController.java
+++ src/main/java/com/example/demo/controller/NotesController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,6 +43,12 @@ public class NotesController {
 	@GetMapping("/api/notes/{id}")
 	public ResponseEntity<Note> readNote(@PathVariable Long id){
 		return new ResponseEntity<Note>(nRepo.findById(id).get(),HttpStatus.OK);
+	}
+	@DeleteMapping("/api/notes/{id}")
+	public ResponseEntity<HttpStatus> deleteNote(@PathVariable Long id){
+		nRepo.deleteById(id);
+		return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
+		
 	}
 
 }
